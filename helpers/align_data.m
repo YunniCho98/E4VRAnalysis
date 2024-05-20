e288f99ae50b406de0ea454e28dda7134e41faa1
Yunni Cho <150165856+YunniCho98@users.noreply.github.com>
2024-05-20 08:15:25 +0200

    Update align_data.m

--- helpers/align_data.m
+++ helpers/align_data.m
@@ -1,3 +1,19 @@
+% Copyright (c) 2024 Yunni Cho, EPFL
+% =======================================================================
+% This program is free software: you can redistribute it and/or
+% modify it under the terms of theGNU Lesser General Public License
+% as published by the Free Software Foundation, either version 3 of
+% the License, or (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+% Lesser General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <https://www.gnu.org/licenses/>.
+% ======================================================================
+
 %align_data - Aligns the size of the data vector to the size of timestamps.
 % If the data vector is larger, it truncates the excess values.
 % If the data vector is smaller, it pads with NaNs.
