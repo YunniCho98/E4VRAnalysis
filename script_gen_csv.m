51663d21e6b394c95681f7c0fce7421879e72a6f
Yunni Cho <150165856+YunniCho98@users.noreply.github.com>
2024-05-20 08:13:55 +0200

    Update script_gen_csv.m

--- script_gen_csv.m
+++ script_gen_csv.m
@@ -1,3 +1,19 @@
+% Copyright (c) 2024 Yunni Cho, EPFL
+% =======================================================================
+% This program is free software: you can redistribute it and/or
+% modify it under the terms of theGNU Lesser General Public License
+% as published by the Free Software Foundation, either version 3 of
+% the License, or (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+% Lesser General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <https://www.gnu.org/licenses/>.
+% ======================================================================
+
 %script_gen_csv.m: Script to generate CSV files from Empatica E4 and Pico Neo 3 Pro data
 %
 % This script processes Empatica E4 and Pico Neo 3 Pro data to generate CSV files for further analysis.
