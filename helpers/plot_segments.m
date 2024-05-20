ed5ee7d27a0de038a0aa6c45d9fa54a9cb344e56
Yunni Cho <150165856+YunniCho98@users.noreply.github.com>
2024-05-20 08:15:49 +0200

    Update plot_segments.m

--- helpers/plot_segments.m
+++ helpers/plot_segments.m
@@ -1,3 +1,19 @@
+% Copyright (c) 2024 Yunni Cho, EPFL
+% =======================================================================
+% This program is free software: you can redistribute it and/or
+% modify it under the terms of theGNU Lesser General Public License
+% as published by the Free Software Foundation, either version 3 of
+% the License, or (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+% Lesser General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <https://www.gnu.org/licenses/>.
+% ======================================================================
+
 %plot_segments - Plots data with indicators for sequence changes. The data itself is not segmented, we
 % just plot all the data as is with vertical lines indicating where the sequence changes occur.
 %
